491d201262a10c850784311ac01c2f58f721c48e
Indie Breath <breathindie@gmail.com>
2024-02-19 20:51:46 +1100

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<main>
+		<header>
+			<table>
+				<tr>
+					<td class="border-2 border-solid border-black w-1/3"></td>
+					<td class="border-2 border-solid border-black w-1/3" ></td>
+					<td class="border-2 border-solid border-black w-1/3"></td>
+				</tr>
+			</table>
+		</header>
+	</main>
+</template>
