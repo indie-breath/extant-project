8ca75629a952a4a51cc4bea60ed098af1897f6ac
Indie Breath <breathindie@gmail.com>
2024-02-21 10:48:00 +1100

    Finished home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,51 @@
 <template>
-	<main class="grid grid-cols-5">
-		<div class="border-black border-solid border-2 h-screen col-span-1"></div>
-		<div id="middle" class="border-black border-solid border-2 flex h-screen col-span-3 bg-[url('/Extant_background.jpg')]">
-			<div class="self-center relative mx-auto w-full h-full flex">
+	<div class="grid grid-cols-7">
+		<div class="border-black border-solid border-2 col-span-1"></div>
+
+		<div id="middle" class="col-span-5 h-fit bg-[url('/Extant_background.jpg')]">
+			<div class="relative m-auto w-full">
 				<img src="/Extant_logo.jpg" width="200" class="absolute">
 				<div class="self-center mx-auto w-5/6 h-5/6 bg-white">
-					<p class="text-right text-red-700 m-5">SITE NOTICE ##/##/####. ExTant Services are indefinitely dicontinued.</p>
+					<p class="text-center text-red-700 m-5">SITE NOTICE ##/##/####. ExTant Services are indefinitely dicontinued.</p>
 
 					<div class="grid grid-cols-5 my-10">
 						<div class="col-span-1"></div>
-						<h1 class="col-span-2 text-center"> Against All Odds!</h1>
-						<form action="" class="grid grid-cols-2">
-							<input type="text" placeholder="Search..."></input>
+						<h1 class="col-span-2 text-center text-2xl"> Against All Odds!</h1>
+						<form action="" class="col-span-2">
+							<input type="text" placeholder="Search..." class="border-solid border-2 border-black"></input>
 							<input type="submit" value="Search">
 						</form>
 					</div>
 
 					<ul class="list-none my-10 mx-0 p-0 overflow-hidden">
-						<li class="float-right block text-center my-1 mr-2 py-5 px-10 no-underline bg-[url('/image3.jpg')]">Knick Knacks</li>
-						<li class="float-right block text-center my-1 mr-1 py-5 px-8 no-underline bg-[url('/image2.jpg')]">Clothing & Gear</li>
-						<li class="float-right block text-center my-1 mr-1 py-5 px-5 no-underline bg-[url('/image1.jpg')]">Sports & Activities</li>
-						<li class="float-right block text-center my-1 mr-1 py-5 px-7 no-underline bg-[url('/image0.jpg')]" >Books & Comics</li>
+						<li class="float-right block text-center mx-2 py-5 px-10 no-underline bg-[url('/image3.jpg')]">Knick Knacks</li>
+						<li class="float-right block text-center mx-2 py-5 px-8 no-underline bg-[url('/image2.jpg')]">Clothing & Gear</li>
+						<li class="float-right block text-center mx-2 py-5 px-5 no-underline bg-[url('/image1.jpg')]">Sports & Activities</li>
+						<li class="float-right block text-center mx-2 py-5 px-7 no-underline bg-[url('/image0.jpg')]" >Books & Comics</li>
 					</ul>
 
-					<ul class="list-non my-10 mx-0 p-0 overflow-hidden">
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
+					<ul class="list-none my-10 mx-0 p-0 overflow-hidden">
+						<li class="float-left block no-underline mr-5 ml-10"><img src="/image_row2.jpg" width=125></li>
+						<li class="float-left block no-underline mx-5"><img src="/image_row1.jpg" width=125></li>
+						<li class="float-left block no-underline mx-5"><img src="/image_row2.jpg" width=125></li>
+						<li class="float-left block no-underline mx-5"><img src="/image_row1.jpg" width=125></li>
+						<li class="float-left block no-underline mx-5"><img src="/image_row0.jpg" width=125></li>
 					</ul>
+
+					<div class="grid grid-cols-2">
+						<p class="mx-10 float-left bg-[url('/image_bg1.jpg')] bg-contain bg-no-repeat p-5 text-cyan-400">
+						SUBSCRIBE to ExTant's <br> BUMPIN' HOT newsletter to <br> get notified of our radical <br> sales, discounts and new <br> products EARLY! <br><br> - ExTant site news and updates <br> ##/##/#### We at ExTant are <br> very sorry to inform our loyal <br> customers that our EShopping <br> website and business will be <br> indefinitely closed. <br><br> Thank you for using our se...
+						</p>
+						<img src="/image4.jpg" class="float-right -mx-10">
+					</div>
+
+					<p class="text-center">Click here for more information of ExTant <br> inc. customer support, business line.</p>
 				</div>
 			</div>
 		</div>
-		<div class="border-black border-solid border-2 h-screen col-span-1">
-			<img src="/Extant_Tasc_2.png" class="my-auto">
-		</div>
-	</main>
 
-	<footer>
-		<p class="text-center">Copyright Extant 2024</p>
-	</footer>
+		<div class="border-black border-solid border-2 col-span-1">
+			<img src="/Extant_Tasc_2.png" class="mr-auto">
+		</div>
+	</div>
 </template>
