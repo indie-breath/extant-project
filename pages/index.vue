13f8314b1af0dafcc61c27fe39901a39580b8f66
Indie Breath <breathindie@gmail.com>
2024-02-20 20:00:43 +1100

    Added custom error page

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
 	<header>
 		<h1 class="text-2xl text-center p-5">Shopping ARG</h1>
 			<ul id="navbar" class="list-non m-0 p-0 overflow-hidden bg-slate-700">
-			<li class="float-left"><a class="block text-white text-center px-5 py-3 no-underline hover:bg-slate-600" href="">Home</a></li>
-			<li class="float-left"><a class="block text-white text-center px-5 py-3 no-underline hover:bg-slate-600" href="">About</a></li>
-			<li class="float-left"><a class="block text-white text-center px-5 py-3 no-underline hover:bg-slate-600" href="">Other</a></li>
+			<li class="float-left"><NuxtLink class="block text-white text-center px-5 py-3 no-underline hover:bg-slate-600" to="/">Home</NuxtLink></li>
+			<li class="float-left"><NuxtLink class="block text-white text-center px-5 py-3 no-underline hover:bg-slate-600" to="/about">About</NuxtLink></li>
+			<li class="float-left"><NuxtLink class="block text-white text-center px-5 py-3 no-underline hover:bg-slate-600" to="/other">Other</NuxtLink></li>
 		</ul>
 	</header>
 
