7af75e9dac40d833a5ee90bc69875a60654fd58f
Indie Breath <breathindie@gmail.com>
2024-02-20 19:00:26 +1100

    Created Home Page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,20 @@
 <template>
-	<main>
-		<header>
-			<table>
-				<tr>
-					<td class="border-2 border-solid border-black w-1/3"></td>
-					<td class="border-2 border-solid border-black w-1/3" ></td>
-					<td class="border-2 border-solid border-black w-1/3"></td>
-				</tr>
-			</table>
-		</header>
+	<header>
+		<h1 class="text-2xl text-center p-5">Shopping ARG</h1>
+			<ul id="navbar" class="list-non m-0 p-0 overflow-hidden bg-slate-700">
+			<li class="float-left"><a class="block text-white text-center px-5 py-3 no-underline hover:bg-slate-600" href="">Home</a></li>
+			<li class="float-left"><a class="block text-white text-center px-5 py-3 no-underline hover:bg-slate-600" href="">About</a></li>
+			<li class="float-left"><a class="block text-white text-center px-5 py-3 no-underline hover:bg-slate-600" href="">Other</a></li>
+		</ul>
+	</header>
+
+	<main class="grid grid-cols-5">
+		<div class="border-black border-solid border-2 h-screen col-span-1"></div>
+		<div class="border-black border-solid border-2 h-screen col-span-3"></div>
+		<div class="border-black border-solid border-2 h-screen col-span-1"></div>
 	</main>
+
+	<footer>
+		<p class="text-center">Copyright [Company Name] 2024</p>
+	</footer>
 </template>
